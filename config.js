daa16330828dbefac18c5a474ace3afe374a16b2
RicardoSolorzano0 <ricardosolorzano97@gmail.com>
2022-01-28 22:14:16 -0600

    server funcionando

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //datos que necesitaremos para monstar nuestro servidor
-const API_VERSION = "v1";
+const API_VERSION = "v3";
 const IP_SERVER = "localhost";
 const PORT_DB = 27017;
 //exportamos una constante
