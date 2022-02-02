7cdbe45ec9864a00bb32d3efe1247f6f37a6ce2b
RicardoSolorzano0 <ricardosolorzano97@gmail.com>
2022-02-02 14:08:41 -0600

    ingreso de usuarios
    prueba de api de usuarios

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const { API_VERSION } = require("./config");
 
 //cargaremos rutas
+const userRoutes = require("./routers/user");
 
 //lo trabajaremos mas adelante
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +17,7 @@ app.use(bodyParser.json());
 //configure header http
 
 //router basic
-
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 //exportamos las configuraciones que necesitamos
 module.exports = app;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = process.env.PORT || 3977;//ip donde corre el server
+const port = process.env.PORT || 3977;//ip donde corre el server    
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
 
